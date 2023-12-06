3df0a522d9233715920505cc7b8ab13418b699cf
Tushar <bhanottushar1603@gmail.com>
2023-12-05 22:49:04 -0500

    populated with new code files

--- ASSIGNMENT02/app.js
+++ ASSIGNMENT02/app.js
@@ -1,15 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoUsername = process.env.MONGODB_USERNAME;
+const mongoPassword = process.env.MONGODB_PASSWORD;
 
+mongoose.connect('mongodb+srv://200515111:Georgian1@recipebook.s5w3nzc.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+const recipeSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+// Express App Setup
 const app = express();
 
-// view engine setup
+// View Engine Setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -19,27 +38,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+// Start the Server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -1,41 +1,21 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chefs Book' });
-});
-
-router.get('/recipe1', function(req, res, next) {
-  res.render('recipe1', { title: 'Paneer Lababdaar' });
-});
-
-router.get('/recipe2', function(req, res, next) {
-  res.render('recipe2', { title: 'a' });
-});
-
-router.get('/recipe3', function(req, res, next) {
-  res.render('recipe3', { title: 'b' });
-});
-
-router.get('/recipe4', function(req, res, next) {
-  res.render('recipe4', { title: 'c' });
-});
-
-router.get('/recipe5', function(req, res, next) {
-  res.render('recipe5', { title: 'd' });
-});
-
-router.get('/recipe6', function(req, res, next) {
-  res.render('recipe6', { title: 'e' });
+  res.render('index', { title: 'Express' });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'f' });
+  res.render('contact', { title: 'Express' });
 });
 
-router.get('/add', function(req, res, next) {
-  res.render('add', { title: 'g' });
+router.get('/user', function(req, res, next) {
+  res.render('user', { title: 'Express' });
+});
+
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
